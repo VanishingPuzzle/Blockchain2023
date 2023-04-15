efa6c42f113d7d4b17d12bd0e9db4d404373d8ea
deliveredmetal <118897748+DavidDMT12@users.noreply.github.com>
2023-04-15 16:10:54 +0200

    Update Distance to center.py

--- Distance to center.py
+++ Distance to center.py
@@ -19,3 +19,19 @@ df['distance_to_origin'] = df['coordinates'].apply(euclidean_distance)
 df = df.drop('coordinates', axis=1)
 
 print(df)
+
+# for already separated coordinates
+import pandas as pd
+import numpy as np
+
+# create example dataframe with x,y coordinates (as floating point values)
+df = pd.DataFrame({'X': [3.4, 4.2, -2.1, -1.5], 'Y': [4.1, 3.8, -1.3, -2.7]})
+
+# define a function to calculate euclidean distance
+def euclidean_distance(x, y):
+    return np.sqrt(x**2 + y**2)
+
+# calculate distance to origin and create a new column
+df['distance_to_origin'] = df.apply(lambda row: euclidean_distance(row['X'], row['Y']), axis=1)
+
+print(df)
